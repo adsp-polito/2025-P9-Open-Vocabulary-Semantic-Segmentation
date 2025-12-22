ed87e0a533c91ecaaa22811c8a42346621aaab4e
abedashi <abed_ashi@hotmail.com>
2025-12-22 19:28:41 +0100

    run script

--- scripts/run.sh
+++ scripts/run.sh
@@ -4,11 +4,10 @@ export RSIB_CKPT='dinov3/vitl16-sat493m/dinov3_vitl16_pretrain_sat493m-eadcf0ff.
 
 RESULTS='output/floodnet_training'
 
-sh scripts/train.sh configs/rn101_224.yaml 0 $RESULTS \
+sh scripts/train.sh configs/rn101_224.yaml 1 $RESULTS \
 SOLVER.IMS_PER_BATCH 4 \
 SOLVER.MAX_ITER 30002 \
 DATALOADER.NUM_WORKERS 8 \
 
-sh scripts/eval.sh configs/rn101_224.yaml 0 $RESULTS/Eval \
-MODEL.WEIGHTS $RESULTS/model_0029999.pth
-
+sh scripts/eval.sh configs/rn101_224.yaml 1 $RESULTS \
+MODEL.WEIGHTS $RESULTS/model_final.pth
