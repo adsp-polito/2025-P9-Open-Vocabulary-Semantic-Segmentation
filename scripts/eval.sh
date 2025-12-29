e07fdfdfb452b532c3bbb7d75787c0e74a83c58c
abedashi <abed_ashi@hotmail.com>
2025-12-29 08:18:14 +0100

    Update RSIB_CKPT path

--- scripts/eval.sh
+++ scripts/eval.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 export DETECTRON2_DATASETS='gs_net/data/datasets'
 # export RSIB_CKPT='dinov3/vitl16-sat493m/dinov3_vitl16_pretrain_sat493m-eadcf0ff.pth'
-export RSIB_CKPT='gs_net/third_party/finetune_landdiscover_seg_epoch_5.pth'
+export RSIB_CKPT='gs_net/third_party/finetune_landdiscover_seg_epoch_10.pth'
 
 config=$1
 gpus=$2
