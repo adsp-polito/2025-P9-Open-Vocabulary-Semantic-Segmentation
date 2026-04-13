b2a080d884c299f7c6653fd32f9d876435508330
Abed Ashi <abed@ustyle.it>
2026-04-13 21:55:33 +0200

    .

--- gs_net/third_party/setup.sh
+++ gs_net/third_party/setup.sh
@@ -7,10 +7,10 @@ pip install git+https://github.com/openai/CLIP.git
 pip install git+https://github.com/facebookresearch/detectron2.git
 
 echo "=== Cloning Talk2DINO-ViTB from HuggingFace ==="
-if [ -d "/Talk2DINO-ViTB" ]; then
+if [ -d "Talk2DINO-ViTB" ]; then
     echo "Talk2DINO-ViTB already exists, skipping clone."
 else
-    git clone https://huggingface.co/lorebianchi98/Talk2DINO-ViTB /Talk2DINO-ViTB
+    git clone https://huggingface.co/lorebianchi98/Talk2DINO-ViTB Talk2DINO-ViTB
 fi
 
 echo "=== Done! Now run: python eval_talk2dino.py ==="
